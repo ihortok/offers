1a588a6b8c05f6767c2b7a635c81229e6db09307
ihortok <itokaryk@gmail.com>
2023-11-11 13:35:43 +0200

    action text

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import "trix"
+import "@rails/actiontext"
+
+window.addEventListener("trix-file-accept", function(event) {
+  event.preventDefault()
+  alert("File attachment not supported!")
+})
