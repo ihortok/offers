5eec59009aab009f98dd511944d0d1628ab6ab0f
ihortok <itokaryk@gmail.com>
2023-11-24 16:49:40 +0100

    offer card: display state

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -2,4 +2,11 @@ module OffersHelper
   def offer_time_for(offer)
     "#{offer.start_at} - #{offer.end_at}"
   end
+
+  def offer_state_tag_classes_for(offer)
+    return 'tag-warning' if offer.draft?
+    return 'tag-secondary' if offer.archived?
+
+    'tag-success'
+  end
 end
