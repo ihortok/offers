69a06e6cb96ab4ea8ae951fb00502f7d3d9f32e3
ihortok <itokaryk@gmail.com>
2023-11-25 12:50:42 +0100

    rename offer columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_24_155702) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_25_113959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
@@ -79,13 +79,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_24_155702) do
   end
 
   create_table "offers", force: :cascade do |t|
-    t.string "what"
+    t.string "title"
     t.datetime "start_at"
     t.datetime "end_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "offerer_id", null: false
-    t.string "where"
+    t.string "place"
     t.uuid "uuid", default: -> { "uuid_generate_v4()" }, null: false
     t.string "aasm_state"
     t.index ["offerer_id"], name: "index_offers_on_offerer_id"
